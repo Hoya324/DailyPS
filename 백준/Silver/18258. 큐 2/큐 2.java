2de1be01aea15c3aa5ccd64a37a3fcc0bdc8055c
나경호 <gangho324@gmail.com>
2024-04-12 21:59:07 +0900

    [Silver IV] Title: 큐 2, Time: 1128 ms, Memory: 370932 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private String solve() throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        Deque<String> deque = new ArrayDeque<>();
+        StringTokenizer command;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int commandNumber = Integer.parseInt(bufferedReader.readLine());
+        for (int count = 0; count < commandNumber; count++) {
+            command = new StringTokenizer(bufferedReader.readLine());
+            switch (command.nextToken()) {
+                case "push":
+                    deque.add(command.nextToken());
+                    break;
+
+                case "pop":
+                    if (deque.isEmpty()) {
+                        stringBuilder.append("-1").append("\n");
+                    } else {
+                        stringBuilder.append(deque.pop()).append("\n");
+                    }
+                    break;
+
+                case "size":
+                    stringBuilder.append(deque.size()).append("\n");
+                    break;
+
+                case "empty":
+                    if (deque.isEmpty()) {
+                        stringBuilder.append(1).append("\n");
+                    } else {
+                        stringBuilder.append(0).append("\n");
+                    }
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        stringBuilder.append(-1).append("\n");
+                        break;
+                    }
+                    stringBuilder.append(deque.peek()).append("\n");
+                    ;
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        stringBuilder.append(-1).append("\n");
+                        break;
+                    }
+                    stringBuilder.append(deque.peekLast()).append("\n");
+                    break;
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        System.out.println(main.solve());
+    }
+}
