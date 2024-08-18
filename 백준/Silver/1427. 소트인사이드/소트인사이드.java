f2224bf787bdd207ecf9c2dd5552629ca82568e0
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-08-18 16:53:59 +0900

    [Silver V] Title: 소트인사이드, Time: 68 ms, Memory: 11688 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,29 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+	private static String solution(String str) {
+		StringBuilder answer = new StringBuilder();
+		
+		List<Integer> nums = new ArrayList<>();
+		for (int i = 0; i < str.length(); i++) {
+			nums.add(Character.getNumericValue(str.charAt(i)));
+		}
 
-    public static void main(String[] args) throws IOException {
- 
-        BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-        
-        String data = scan.readLine();
-        ArrayList<Integer> arr = new ArrayList<>();
+		nums.sort(Collections.reverseOrder());
+		for (int i : nums) {
+			answer.append(i);
+		}
+		return answer.toString();
+	}
 
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < data.length(); i++) {
-            arr.add(Character.getNumericValue(data.charAt(i)));
-        }
-
-        Collections.sort(arr, Collections.reverseOrder());
-
-        for(int i: arr){
-            sb.append(i);
-        }
-
-        System.out.println(sb);
-    }   
-    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(solution(str));
+		bw.flush();
+		bw.close();
+	}
 }
-
