302503e5cee75825ae7e3211f4f07a9ffc66e118
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-08-24 18:30:27 +0900

    [Silver II] Title: 참외밭, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -4,27 +4,21 @@ import java.util.*;
 public class Main {
 	private final static int LINE_SIZE = 6;
 
-	private static int solution(int K, List<Integer> directions, List<Integer> lengths) {
-		int everyField = -1;
+	private static int solution(int K, List<Integer> lengths) {
+		int totalField = -1;
 
-		int allLinesProduct = 0;
+		int sumOfProductsOfAdjacentSides = 0;
 		for (int i = 0; i < LINE_SIZE; i++) {
-			if (i == LINE_SIZE - 1) {
-				int nowField = lengths.get(i) * lengths.get(0);
-				allLinesProduct += nowField;
-				if (everyField < nowField) {
-					everyField = nowField;
-				}
-				break;
-			}
-			int nowField = lengths.get(i) * lengths.get(i+1);
-			allLinesProduct += nowField;
-			if (everyField < nowField) {
-				everyField = nowField;
+			int nextIndex = (i + 1) % LINE_SIZE;
+
+			int currentField = lengths.get(i) * lengths.get(nextIndex);
+			sumOfProductsOfAdjacentSides += currentField;
+			if (totalField < currentField) {
+				totalField = currentField;
 			}
 		}
 
-		int melonField = allLinesProduct - (2 * everyField);
+		int melonField = sumOfProductsOfAdjacentSides - (2 * totalField);
 
 		return melonField * K;
 	}
@@ -32,18 +26,17 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int K = Integer.parseInt(br.readLine());
-		List<Integer> directions = new ArrayList<>();
 		List<Integer> lengths = new ArrayList<>();
 
 		StringTokenizer st;
 		for (int i = 0; i < 6; i++) {
 			st = new StringTokenizer(br.readLine());
-			directions.add(Integer.parseInt(st.nextToken()));
+			st.nextToken();
 			lengths.add(Integer.parseInt(st.nextToken()));
 		}
 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(String.valueOf(solution(K, directions, lengths)));
+		bw.write(String.valueOf(solution(K, lengths)));
 		bw.flush();
 		bw.close();
 	}
