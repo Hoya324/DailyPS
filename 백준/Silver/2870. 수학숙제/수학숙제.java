0faf6924fad3aeaf6a3dab9c299aae3ba5634393
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-08-30 00:56:23 +0900

    [Silver IV] Title: 수학숙제, Time: 84 ms, Memory: 12412 KB -BaekjoonHub

---
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+
+	private static String solution(String[] arr) {
+
+		ArrayList<BigInteger> result = new ArrayList<>();
+
+		for (int i = 0; i < arr.length; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < arr[i].length(); j++) {
+
+				if (Character.isDigit(arr[i].charAt(j))) {
+
+					sb.append(arr[i].charAt(j));
+
+					if (j == arr[i].length() - 1) {
+						result.add(new BigInteger(sb.toString()));
+						sb.delete(0, sb.length());
+					}
+				}
+
+				else {
+					if (sb.length() != 0) {
+						result.add(new BigInteger(sb.toString()));
+						sb.delete(0, sb.length());
+					}
+				}
+
+			}
+		}
+
+		Collections.sort(result);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < result.size(); i++) {
+			sb.append(result.get(i)).append("\n");
+		}
+		return sb.toString();
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		String[] arr = new String[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = br.readLine();;
+		}
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(String.valueOf(solution(arr)));
+		bw.flush();
+		bw.close();
+	}
+}
