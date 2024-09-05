5291efbf416c95557f759ad5a10122f76d2cd946
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-09-05 15:29:57 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 120 ms, Memory: 16644 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,60 +1,61 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
+	private static final String NO = "no";
+	private static final String YES = "yes";
+	private static final char LEFT_PARENTHESES = '(';
+	private static final char RIGHT_PARENTHESES = ')';
+	private static final char LEFT_SQUARE_BRACKET = '[';
+	private static final char RIGHT_SQUARE_BRACKET = ']';
+	private static final String FULL_STOP = ".";
 
-    public static String solution(String word) {
-        char nowChar;
-        Stack<Character> stack = new Stack<>();
+	private static String solution(String word) {
+		Deque<Character> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < word.length()-1; i++) {
-            nowChar = word.charAt(i);
-            if (nowChar == '(') {
-                stack.push('(');
-            }
-            else if (nowChar == '[') {
-                stack.push('[');
-            }
+		for (int i = 0; i < word.length() - 1; i++) {
+			char currentCharUnit = word.charAt(i);
 
-            else if (nowChar == ')') {
-                 if(!stack.empty() && stack.peek() == '(') {
-                     stack.pop();
-                 }
-                 else {
-                     return "no";
-                 }
-            }
+			if (currentCharUnit == LEFT_PARENTHESES) {
+				stack.push(LEFT_PARENTHESES);
+			} else if (currentCharUnit == LEFT_SQUARE_BRACKET) {
+				stack.push(LEFT_SQUARE_BRACKET);
+			} else if (currentCharUnit == RIGHT_PARENTHESES) {
+				if (!stack.isEmpty() && stack.peek() == LEFT_PARENTHESES) {
+					stack.pop();
+				} else {
+					return NO;
+				}
+			} else if (currentCharUnit == RIGHT_SQUARE_BRACKET) {
+				if (!stack.isEmpty() && stack.peek() == LEFT_SQUARE_BRACKET) {
+					stack.pop();
+				} else {
+					return NO;
+				}
+			}
 
-            else if (nowChar == ']'){
-                if (!stack.empty() && stack.peek() == '[') {
-                    stack.pop();
-                }
-                else {
-                    return "no";
-                }
-            }
+		}
+		if (stack.isEmpty()) {
+			return YES;
+		}
+		return NO;
+	}
 
-        }
-        if (stack.empty()) {
-            return "yes";
-        }
-        return "no";
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
-
-        while (true) {
-            String word = br.readLine();
-            if (word.equals(".")) {
-                break;
-            }
-            stringBuilder.append(solution(word)).append("\n");
-        }
-        System.out.println(stringBuilder);
-    }
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		while (true){
+			String nowWord = br.readLine();
+			if (nowWord.equals(FULL_STOP)) {
+				break;
+			}
+			sb.append(solution(nowWord)).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
