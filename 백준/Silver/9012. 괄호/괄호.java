808d51d2724ebf878b98519a4994e19a9ed4f325
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-09-05 15:08:58 +0900

    [Silver IV] Title: 괄호, Time: 76 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,43 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
+	private static final String NO = "NO";
+	private static final String YES = "YES";
+	private static final char RIGHT_PARENTHESES = ')';
 
+	private static String solution(String parentheses) {
+		Deque<Character> stack = new ArrayDeque<>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
+		for (int index = 0; index < parentheses.length(); index++) {
+			char currentParentheses = parentheses.charAt(index);
+			if (stack.isEmpty()) {
+				if (currentParentheses == RIGHT_PARENTHESES) {
+					return NO;
+				}
+				stack.push(currentParentheses);
+			} else {
+				if (currentParentheses != stack.peek()) {
+					stack.pop();
+				} else {
+					stack.push(currentParentheses);
+				}
+			}
+		}
 
-        int num = Integer.parseInt(br.readLine());
+		if (stack.isEmpty()) {
+			return YES;
+		}
+		return NO;
+	}
 
-        for (int i = 0; i < num; i++) {
-            String ps = br.readLine();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		int testCase = Integer.parseInt(br.readLine());
 
-            for (int j = 0; j < ps.length(); j++) {
-                if (ps.charAt(j) == '(') {
-                    stack.push('(');
-                }
-                else {
-                    if (stack.empty()) {
-                        stack.push('n');
-                        break;
-                    }
-                    stack.pop();
-                }
-            }
-
-            if (stack.empty()) {
-                sb.append("YES\n");
-            }
-            else {
-                sb.append("NO\n");
-            }
-            stack.clear();
-        }
-        System.out.println(sb);
-    }
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < testCase; i++) {
+			sb.append(solution(br.readLine())).append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
