8e47372ce940a50fcfc17a416ade6be572a0ef00
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-09-08 00:01:17 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14880 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,75 +1,62 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    
-    
-    
-	public static void main(String[] args) throws IOException{
+public class Main {
+	
+	private static final int IMPORTANCE = 0; 
+	private static final int INIT_INDEX = 1; 
 
-    BufferedReader scan = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static int solution(BufferedReader br) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int length = Integer.parseInt(st.nextToken());
+		int targetIndex = Integer.parseInt(st.nextToken());
 
-        
-    Queue<Integer> queue = new LinkedList<>();
+		// 큐에 문서들의 중요도와 초기 위치를 함께 저장
+		Queue<int[]> queue = new LinkedList<>();
+		String[] temp = br.readLine().split(" ");
+		for (int i = 0; i < length; i++) {
+			int[] importanceAndInitPoint = {Integer.parseInt(temp[i]), i};
+			queue.add(importanceAndInitPoint);
+		}
 
-    int N = Integer.parseInt(scan.readLine());    
+		// 중요도를 내림차순으로 정렬하는 우선순위 큐
+		PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+		for (String doc : temp) {
+			priorityQueue.add(Integer.parseInt(doc));
+		}
 
-    StringBuilder sb = new StringBuilder();
-        
-    for (int i = 0; i < N; i++) {
-        
-        StringTokenizer st = new StringTokenizer(scan.readLine());
-        int num = Integer.parseInt(st.nextToken());
-        int index = Integer.parseInt(st.nextToken());
-        
-        st = new StringTokenizer(scan.readLine());
-        for (int j = 0; j < num; j++) {
-            queue.add(Integer.parseInt(st.nextToken()));
-        }
+		int order = 0;
+		while (!queue.isEmpty()) {
+			int[] current = queue.poll();
 
-        int importance = 9;
-        int order = 1;
-        while (importance > 0) {
-            // 해당 중요도가 큐에 들어있을 때
-            if (queue.contains(importance)) {
-                // 해당하는 인덱스의 수가 맨 앞, 중요도가 가장 높음
-                if (index == 0 && queue.peek() == importance) {
-                    sb.append(order + "\n");
-                    break;
-                }    
-                // 해당 인덱스의 수가 맨 앞, 중요도는 가장 높지 않음
-                else if(index == 0 && queue.peek() != importance) {
-                    // 위치 초기화
-                    index = (queue.size() - 1);
-                    queue.add(queue.remove());
-                }                
-                else if (index != 0 && queue.peek() == importance){
-                    queue.remove();
-                    // 해당 인덱스 위치 저장
-                    index--;
-                    // 순서
-                    order++;                    
-                    continue;
-                }
-                else {
-                    // 중요도가 가장 높은 문서를 앞으로 옮기는 중
-                    queue.add(queue.remove());
-                    // 해당 인덱스 위치 저장
-                    index--;
-                }
-                
-                
-            }
-            // 테스크 케이스에 포함된 중요도 중 큰 순서대로 확인
-            else {
-                importance--;
-            }
-        }
-        queue.clear();
-    }
-    bw.write(sb.toString());
-    bw.flush();
-    bw.close();
-    }
-}   
+			// 현재 문서가 가장 높은 중요도를 가지는 경우 출력
+			if (current[IMPORTANCE] == priorityQueue.peek()) {
+				order++;
+				priorityQueue.poll(); // 해당 중요도 제거
+				if (current[INIT_INDEX] == targetIndex) {
+					return order;
+				}
+			} else {
+				// 중요도가 높지 않으면 다시 큐의 끝에 추가
+				queue.add(current);
+			}
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int count = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+			sb.append(solution(br)).append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
