6545be732f6530551a7186e68ea85635b745debe
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-09-29 16:24:15 +0900

    [Silver II] Title: 외판원 순회 2, Time: 120 ms, Memory: 13592 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static int[][] W;
+	static boolean[] visited;
+	static int minCost = Integer.MAX_VALUE;
+
+	static void DFS(int currentCity, int visitedCityCount, int currentCost, int start) {
+		if (visitedCityCount == N) {
+			if (W[currentCity][start] != 0) {
+				minCost = Math.min(minCost, currentCost + W[currentCity][start]);
+			}
+			return;
+		}
+
+		// 모든 도시를 순회하며 DFS 탐색
+		for (int i = 0; i < N; i++) {
+			if (!visited[i] && W[currentCity][i] != 0) {
+				visited[i] = true;
+				DFS(i, visitedCityCount + 1, currentCost + W[currentCity][i], start); // 다음 도시로 이동
+				visited[i] = false;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		W = new int[N][N];
+		visited = new boolean[N];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				W[i][j] = sc.nextInt();
+			}
+		}
+
+		visited[0] = true;
+		DFS(0, 1, 0, 0);
+
+		System.out.println(minCost);
+	}
+}
