d8eb155547fde7aa4a926550fea9f57057e51e27
나경호(Na Gyeongho) <hoyana1225@gmail.com>
2024-11-29 11:00:15 +0900

    Update 타겟 넘버.java

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,16 +1,16 @@
 class Solution {
     
-    public int dfs(int[] numbers, int index, int currentTotal, int target) {
+    public int getTargetTotal(int[] numbers, int index, int currentTotal, int target) {
         
         if (index == numbers.length) {
             return currentTotal == target ? 1 : 0;
         }
         
-        return dfs(numbers, index + 1, currentTotal + numbers[index], target) + dfs(numbers, index + 1, currentTotal - numbers[index], target);
+        return getTargetTotal(numbers, index + 1, currentTotal + numbers[index], target) + getTargetTotal(numbers, index + 1, currentTotal - numbers[index], target);
     }
     
     public int solution(int[] numbers, int target) {
-        int answer = dfs(numbers, 0, 0, target);
+        int answer = getTargetTotal(numbers, 0, 0, target);
         return answer;
     }
 }
